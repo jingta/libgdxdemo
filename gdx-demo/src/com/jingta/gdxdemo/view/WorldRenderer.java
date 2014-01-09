008415de21f780be25748708e0285658aa38ead6
jingta <jingregister@gmail.com>
2014-01-09 09:23:09 -0500

    updates to worldrenderer for textures

--- gdx-demo/src/com/jingta/gdxdemo/view/WorldRenderer.java
+++ gdx-demo/src/com/jingta/gdxdemo/view/WorldRenderer.java
@@ -1,7 +1,10 @@
 package com.jingta.gdxdemo.view;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Rectangle;
@@ -12,17 +15,54 @@ import com.jingta.gdxdemo.model.World;
 public class WorldRenderer {
 	private World world;
 	private OrthographicCamera cam;
-	
+	private SpriteBatch spriteBatch;
+	private boolean debug = false;
 	ShapeRenderer debugRenderer = new ShapeRenderer(); //debugging?
+	private Texture blockTexture;
+	private Texture heroTexture;
+	private int width;
+	private int height;
+	private float ppux; // pixels per unit, x axis
+	private float ppuy; // pixels per unit, y axis
+	private static final float CAMERA_WIDTH = 10f;
+	private static final float CAMERA_HEIGHT = 7f;
 	
-	public WorldRenderer(World world) {
+	public WorldRenderer(World world, boolean debug) {
 		this.world = world;
-		this.cam = new OrthographicCamera(10, 7);
-		this.cam.position.set(5, 3.5f, 0);
+		this.cam = new OrthographicCamera(CAMERA_WIDTH, CAMERA_HEIGHT);
+		this.cam.position.set(CAMERA_WIDTH/2, CAMERA_HEIGHT/2, 0);
 		this.cam.update();
+		this.debug = debug;
+		spriteBatch = new SpriteBatch();
+		loadTextures();
+	}
+	public void loadTextures() {
+		blockTexture = new Texture(Gdx.files.internal("images/block.png"));
+		heroTexture = new Texture(Gdx.files.internal("images/hero.png"));
+	}
+	public void setSize(int w, int h){
+		this.width = w;
+		this.height = h;
+		this.ppux = (float)width / CAMERA_WIDTH;
+		this.ppuy = (float)height / CAMERA_HEIGHT;
 	}
 	
 	public void render() {
+		spriteBatch.begin();
+		drawBlocks();
+		drawHero();
+		spriteBatch.end();
+		
+		if (debug) drawDebug();
+	}
+	
+	private void drawBlocks(){
+		
+	}
+	private void drawHero(){
+		
+	}
+	private void drawDebug(){
 		// render blocks
 		debugRenderer.setProjectionMatrix(cam.combined);
 		debugRenderer.begin(ShapeType.Line);
